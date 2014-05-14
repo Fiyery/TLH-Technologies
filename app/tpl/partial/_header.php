a7d0c294c231fd31d285f0b0be1d8147fcdc445a
ScottChak <burns.ewan@gmail.com>
2014-05-14 20:51:32 +0200

    Intégration améliorée

--- app/tpl/partial/_header.php
+++ app/tpl/partial/_header.php
@@ -1,3 +1,28 @@
+<div id="HeaderRow" class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-2">
+				<a class="mainlink icon home" href='<?= $root_www ?>'>Accueil</a>
+			</div>
+			<div class="col-xs-2">
+				<a class="mainlink icon contact" href='<?= $root_www ?>contact/'>Contacter</a>
+			</div>
+			<div class="col-xs-2">
+				<a class="mainlink icon help" href='<?= $root_www ?>aide/'>Aide</a>
+			</div>
+			<div class="col-xs-2">
+				<a class="mainlink icon plus unfold small" href='<?= $root_www ?>plus/'>En savoir plus</a>
+				<div class="onHover">
+					<?= $list_menu_links ?>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="icon search"><input type="text"/></div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div id="Banner" class="jumbotron">
 	<div class="container">
 		<div class="row">
@@ -5,49 +30,6 @@
 				<div class="display logo">
 					<div class="begin"></div>
 					<div class="content"></div>
-					<div class="end"></div>
-				</div>
-			</div>
-			<div class="col-xs-4">
-				<div class="display search">
-					<div class="begin"></div>
-					<div class="content">
-						<div class="icon search"><input type="text"/></div>
-					</div>
-					<div class="end"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div id="HeaderRow" class="jumbotron">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-8">
-				<div class="display">
-					<div class="begin"></div>
-					<div class="content">
-						<div class="mainlink">
-							<div class="icon home"></div>
-							<a class="title" href='<?= $root_www ?>'>Accueil</a>
-						</div>
-						<div class="mainlink">
-							<div class="icon contact"></div>
-							<a class="title" href='<?= $root_www ?>contact/'>Contacter</a>
-						</div>
-						<div class="mainlink">
-							<div class="icon help"></div>
-							<a class="title" href='<?= $root_www ?>aide/'>Aide</a>
-						</div>
-						<div class="mainlink">
-							<div class="icon plus"></div>
-							<a class="title" href='<?= $root_www ?>plus/'>En savoir plus</a>
-							<div class="onHover">
-								<?= $list_menu_links ?>
-							</div>
-						</div>
-					</div>
 					<div class="end"></div>
 				</div>
 			</div>
