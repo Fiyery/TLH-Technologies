f066c4b672213b8e50dcb6d414e6f42550415151
ScottChak <burns.ewan@gmail.com>
2014-05-20 09:43:18 +0200

    Modification graphiques

---
+++ app/tpl/partial/_banner.php
@@ -0,0 +1,16 @@
+<div id="Banner" class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4 col-sm-8 col-lg-6">
+				<div class="display logo">
+					<div class="begin"></div>
+					<div class="content">
+						<h1><span class="emphasis">TLH</span> Technologies</h1>
+						<p>Internet de demain</p>
+					</div>
+					<div class="end"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- views/admin-default_action.php
+++ views/admin-default_action.php
@@ -1,16 +1,14 @@
-<div class="content">
-	<div class="center large">
-		<h1>Veuillez saisir vos identifiants</h1>
-		<form action='<?= $root_www ?>admin/connection/' method='post'>
-			<div class="row">
-				<div class="col-xs-4 lbl"><span>Login<span></div>
-				<div class="col-xs-6 col-sm-8"><input name='login' type="text"/></div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 lbl"><span>Mot de passe<span></div>
-				<div class="col-xs-6 col-sm-8"><input name='pass' type="password"/></div>
-			</div>
-			<div class="row"><div class="col-xs-12 submit"><input type='submit' value='Valider'></div></div>
-		</form>
-	</div>
+<div class="content center large">
+	<h1>Veuillez saisir vos identifiants</h1>
+	<form action='<?= $root_www ?>admin/connection/' method='post'>
+		<div class="row">
+			<div class="col-xs-4 lbl"><span>Login<span></div>
+			<div class="col-xs-6 col-sm-8"><input name='login' type="text"/></div>
+		</div>
+		<div class="row">
+			<div class="col-xs-4 lbl"><span>Mot de passe<span></div>
+			<div class="col-xs-6 col-sm-8"><input name='pass' type="password"/></div>
+		</div>
+		<div class="row"><div class="col-xs-10 col-sm-12 submit"><input type='submit' value='Valider'></div></div>
+	</form>
 </div>
