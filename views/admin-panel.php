088fb0c6391461564c8d6ce28cfe1bcbd216cd4c
ScottChak <burns.ewan@gmail.com>
2014-05-25 17:58:45 +0200

    Merge and buf fix

--- views/admin-panel.php
+++ views/admin-panel.php
@@ -21,7 +21,6 @@
 						</tr>
 					</thead>
 					<tbody>
-					
 						<?php foreach ($menus as $item) :?>
 							<tr class="menu">
 								<th><span><?= $item->name ?></span></th>
@@ -29,88 +28,36 @@
 								<th><span><?= $item->date_modification ?></span></th>
 								<th class="fixed">
 									<a href='<?= $root_www ?>admin/edit/?type=sous_menu&id_menu=<?= $item->id ?>' class="icon small plus_full">&nbsp;</a>
-									
 									<?php if ($item->enable) : ?>
-									
 										<a href='<?= $root_www ?>admin/disable/?type=menu&id=<?= $item->id ?>' class="icon small large eye_on">&nbsp;</a>
-									
 									<?php else : ?>
-									
 										<a href='<?= $root_www ?>admin/enable/?type=menu&id=<?= $item->id ?>' class="icon small large eye_off">&nbsp;</a>
-									
 									<?php endif; ?>
-									
 									<a href='<?= $root_www ?>admin/moveup/?type=menu&id=<?= $item->id ?>' class="icon small up">&nbsp;</a>
 									<a href='<?= $root_www ?>admin/movedown/?type=menu&id=<?= $item->id ?>' class="icon small down">&nbsp;</a>
 									<a href='<?= $root_www ?>admin/edit/?type=menu&id=<?= $item->id ?>' class="icon small large edit">&nbsp;</a>
 								</th>
 							</tr>
-							
 							<?php foreach ($sous_menus as $sous_item) :?>
-							
 								<?php if ($sous_item->id_menu == $item->id) :?>
-								
 									<tr class="sous_menu">
 										<td><span><?= $sous_item->name ?></span></td>
 										<td><span><?= $sous_item->date_creation ?></span></td>
 										<td><span><?= $sous_item->date_modification ?></span></td>
 										<td class="fixed">
-									
 											<?php if ($sous_item->enable) : ?>
-										
 												<a href='<?= $root_www ?>admin/disable/?type=sous_menu&id=<?= $sous_item->id ?>' class="icon small large eye_on">&nbsp;</a>
-										
 											<?php else : ?>
-										
 												<a href='<?= $root_www ?>admin/enable/?type=sous_menu&id=<?= $sous_item->id ?>' class="icon small large eye_off">&nbsp;</a>
-										
 											<?php endif; ?>
-											
 											<a href='<?= $root_www ?>admin/moveup/?type=sous_menu&id=<?= $sous_item->id ?>' class="icon small up">&nbsp;</a>
 											<a href='<?= $root_www ?>admin/movedown/?type=sous_menu&id=<?= $sous_item->id ?>' class="icon small down">&nbsp;</a>
 											<a href='<?= $root_www ?>admin/edit/?type=sous_menu&id=<?= $sous_item->id ?>' class="icon small large edit">&nbsp;</a>
 										</td>
 									</tr>
 								<?php endif; ?>
-								
 							<?php endforeach; ?>
-							
-							
 						<?php endforeach; ?>
-						
-<<<<<<< HEAD
-						
-					<?php endforeach; ?>
-					
-				</tbody>
-			</table>
-			
-			<h2>Pages statiques</h2>
-			
-			<table class="fixedColumn">
-				<thead>
-					<tr>
-						<th><span>Nom du menu / sous-menu</span></th>
-						<th><span>Date de création</span></th>
-						<th><span>Date de modification</span></th>
-						<th class="fixed">
-							<a href='<?= $root_www ?>admin/edit/?type=static_menu' class="icon small plus_full">&nbsp;</a>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-				
-					<?php foreach ($static_menus as $item) :?>
-						<tr class="menu">
-							<th><span><?= $item->name ?></span></th>
-							<th><span><?= $item->date_creation ?></span></th>
-							<th><span><?= $item->date_modification ?></span></th>
-							<th class="fixed">			
-								<a href='<?= $root_www ?>admin/edit/?type=static_menu&id=<?= $item->id ?>' class="icon small large edit">&nbsp;</a>
-							</th>
-						</tr>						
-					<?php endforeach; ?>
-=======
 					</tbody>
 				</table>
 			</div>
@@ -130,8 +77,6 @@
 						</tr>
 					</thead>
 					<tbody>
->>>>>>> origin/master
-					
 						<?php foreach ($static_menus as $item) :?>
 							<tr class="menu">
 								<th><span><?= $item->name ?></span></th>
@@ -142,7 +87,6 @@
 								</th>
 							</tr>						
 						<?php endforeach; ?>
-						
 					</tbody>
 				</table>
 			</div>
